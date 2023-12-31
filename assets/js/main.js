d2b79c20fface1a7d4919d3e8fd58029eba57268
Kesit Budi Kusumo <kesit.budi@bankmandiri.co.id>
2023-12-31 11:22:51 +0700

    Fix: close sidebar menu when any links inside of it clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,8 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
+const navLinks = document.querySelectorAll('.nav__link');
+const hireMeButton = document.getElementById('hire-me-button');
 
 if (navToggle) {
     navToggle.addEventListener('click', () => {
@@ -25,3 +27,15 @@ if (navClose) {
         navMenu.classList.remove('show-menu');
     });
 }
+
+if (hireMeButton) {
+    hireMeButton.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+navLinks.forEach((navLink) => {
+   navLink.addEventListener('click', () => {
+       navMenu.classList.remove('show-menu');
+   });
+});
